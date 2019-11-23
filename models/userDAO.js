f5fd705c51ed2f85e6685512c4e230e5af355d7c
Pedro <pmonteiro1999@gmail.com>
2019-11-23 11:47:38 +0000

    Updated Login with Ajax call

--- models/userDAO.js
+++ models/userDAO.js
@@ -13,20 +13,24 @@ module.exports.getAll = function (callback, next) {
     })
 }
 
-module.exports.login = function (obj,callback, next) {
+module.exports.login = function (obj, callback, next) {
     mysql.getConnection(function (err, conn) {
         if (err) {
             conn.release();
             next(err);
         }
-        else conn.query("Select Username,Password from Cliente where Username=? and Password=?",[obj.Username,obj.Password],function (err, rows) {
+        else conn.query("Select Username,Password from Cliente where Username=? and Password=?", [obj.Username, obj.Password], function (err, rows) {
             console.log(rows);
             console.log(obj);
             conn.release();
-            if (!rows.length === 0)
-                callback({code: 200, status: "ok"},rows);
-            else
-                callback({code: 401, status: "User or password incorrects"},null);
+            if (!(rows.length === 0)) {
+                console.log(rows.length)
+                callback({ code: 200, status: "ok" }, rows);
+            }
+            else {
+                console.log('Erro')
+                callback({ code: 401, status: "User or password incorrects" }, null);
+            }
         })
     })
 }
@@ -35,10 +39,11 @@ module.exports.register = function (obj, callback, next) {
     mysql.getConnection(function (err, conn) {
         if (err) {
             conn.release();
-            next(err); }
-        else conn.query('INSERT INTO Cliente(Username, Email,Password,Pacote_idPacote) VALUES (?,?,?,?)',[obj.name ,obj.email,obj.password,obj.idpacote], function (err, rows) {
-                conn.release();
-                callback(rows);
-            })
+            next(err);
+        }
+        else conn.query('INSERT INTO Cliente(Username, Email,Password,Pacote_idPacote) VALUES (?,?,?,?)', [obj.name, obj.email, obj.password, obj.idpacote], function (err, rows) {
+            conn.release();
+            callback(rows);
+        })
     })
 }
